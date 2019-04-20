638f5f5b43e7032ca1cad4a68964f694c8fef6bf
Raymond Truong <raylintruong@gmail.com>
2019-04-20 02:40:14 -0400

    schema added

--- src/server/api/apiControl.js
+++ src/server/api/apiControl.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 const schemaControl = require('../db/schemaControl');
 const secrets = require('./secrets.json');
 
-// api functions
+
+mongoose.connect(url, {
+  useNewUrlParser: true
+});
+
+let db = mongoose.connection;
+mongoose.connection.once('open', () => {console.log("Database Connection Successful")});
 
 let apiControl = {
+
 }
 
 module.exports = apiControl;
