9a431e8b7397d61b9c48cb2e41985582549fe255
asionnn <collin.li0499@gmail.com>
2019-04-20 23:13:19 -0400

    socket.io connection initialized

--- src/client/App.js
+++ src/client/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
 
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:5000/")
+
 class App extends React.Component {
     render() {
         return (
