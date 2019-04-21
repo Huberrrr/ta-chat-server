9a510a8a726299784aa89bc240da3369a24faf86
Huberrrr <hubermjonathan@gmail.com>
2019-04-20 20:59:32 -0400

    added style for the side bar

--- src/client/App.js
+++ src/client/App.js
@@ -6,9 +6,67 @@ class App extends React.Component {
     render() {
         return (
             <div className="app">
-                <h1> MERN Boilerplate </h1>
+                <ChatRoomsMenu />
+                <Messages />
             </div>
         );
+    }
+}
+
+class ChatRoomsMenu extends React.Component {
+    render() {
+        return (
+            <div className="sidebar bg-a">
+                <ChatRoom active={false} />
+                <ChatRoom active={true} />
+                <ChatRoom active={false} />
+                <ChatRoom active={false} />
+                <ChatRoom active={false} />
+                <ChatRoom active={false} />
+            </div>
+        );
+    }
+}
+
+class ChatRoom extends React.Component {
+    render() {
+        return (
+            <div className={this.props.active ? "room bg-3" : "room"}>
+                <span className="room-name">Chat Room</span>
+            </div>
+        );
+    }
+}
+
+class Messages extends React.Component {
+    render() {
+        return (
+            <div className="messages">
+                <Message />
+                <Message />
+                <Message sent />
+                <Message />
+            </div>
+        );
+    }
+}
+
+class Message extends React.Component {
+    render() {
+        if (this.props.sent) {
+            return (
+                <div className="message-sent">
+                    <img className="message-pic" src="https://lh3.googleusercontent.com/-uqmef23Wnp4/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rfJW2ZCXpDsmqVGczmjvxslDWWgWQ/s192-c-mo/photo.jpg" />
+                </div>
+            );
+
+        } else {
+            return (
+                <div className="message-received">
+                    <img className="message-pic" src="https://lh3.googleusercontent.com/-uqmef23Wnp4/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rfJW2ZCXpDsmqVGczmjvxslDWWgWQ/s192-c-mo/photo.jpg" />
+                </div>
+            );
+        }
     }
 }
 
